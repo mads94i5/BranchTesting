f47c6491fb6bce365f15ca1b3f58744ac3b8fa93
mads94i5 <mads94i5@stud.kea.dk>
2022-04-05 09:20:30 +0200

    Added Main.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Fido");
+        Cat cat1 = new Cat("Awful Animal");
+        dog1.makeSound();
+        cat1.makeSound();
+    }
+}
